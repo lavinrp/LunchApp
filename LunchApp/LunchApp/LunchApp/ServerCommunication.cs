8b4cc85dc161c76f8f85cf88f0ac2769fbbbbaf0
lavinrp <lavinrp@gmail.com>
2017-04-09 00:46:41 -0400

    CreatePeople

--- LunchApp/LunchApp/LunchApp/ServerCommunication.cs
+++ LunchApp/LunchApp/LunchApp/ServerCommunication.cs
@@ -7,9 +7,34 @@ using LunchApp.ClassReference;
 namespace LunchApp
 {
     public static class ServerCommunication
-    {
+    { 
+
+        private static List<Order> CreateOrders()
+        {
+            return new List<Order>();
+        }
+    
         private static List<Person> CreatePeople()
         {
+            List<Person> returnPeople = new List<Person>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                string name = "Person Name " + i;
+                Person person = new Person(name);
+
+                if (i > 0)
+                {
+                    //assign debt
+                    person.m_debtors.Add(returnPeople[i - 1].GetID(), i);
+                    returnPeople[i - 1].m_debtees.Add(person.GetID(), i);
+                }
+
+                person.m_savedOrders = CreateOrders();
+
+                returnPeople.Add(person);
+
+            }
             return new List<Person>();
         }
 
@@ -27,21 +52,21 @@ namespace LunchApp
         {
             return new Event("Wednesday Foo", "CHIPOTLE", DateTime.Today);
         }
+        /*
+                public static Group getGroupName(Guid personalID)
+                {
 
-        public static Group getGroupName(Guid personalID)
-        {
-            Group returnGroup = new Group("Cracker Barrel Group");
-            Event returnEvent = new Event("Wednesday Foo", "CHIPOTLE", DateTime.Today);
-            returnGroup.m_events.Add(returnEvent);
-            Person Chris = new Person("Chris");
-            Person Brian = new Person("Brian");
-            Person Ryan = new Person("Ryan");
-            returnGroup.m_groupees.Add(Chris);
-            returnGroup.m_groupees.Add()
-            returnGroup.m_groupees.Add(Ryan);
-            return 
-        }
+                    Group returnGroup = new Group("Cracker Barrel Group");
+                    Event returnEvent = new Event("Wednesday Foo", "CHIPOTLE", DateTime.Today);
+                    returnGroup.m_events.Add(returnEvent);
+                    Person Chris = new Person("Chris");
+                    Person Brian = new Person("Brian");
+                    Person Ryan = new Person("Ryan");
+                    returnGroup.m_groupees.Add(Chris);
+                    returnGroup.m_groupees.Add()
+                    returnGroup.m_groupees.Add(Ryan);
+                    return 
+    }*/
 
-        public static 
     }
 }
