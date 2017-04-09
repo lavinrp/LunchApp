7124a50f5568444ad56efee8b5e4265f0fbce656
gyurgyma <gyurgyma@mail.uc.edu>
2017-04-08 20:42:46 -0400

    Added UML Classes
    N/A

---
+++ LunchApp/LunchApp/LunchApp/ClassReference/Event.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LunchApp.ClassReference
+{
+    public class Event : IDedNamedClass
+    {
+        public string m_location;
+        public DateTime m_time;
+        public Person m_runner;
+
+        public Dictionary<Guid, Order> m_attendeeOrders;
+        public List<Person> m_attendees;
+
+        public Event(string name, string location, DateTime time)
+        {
+            this.m_name = name;
+
+
+            this.m_location = location;
+            this.m_time = time;
+
+            this.m_attendeeOrders = new Dictionary<Guid, Order>();
+            this.m_attendees = new List<Person>();
+        }
+
+        public bool PlaceOrder()
+        {
+            //TODO: Place the order, and manage all the stuff.
+            return true;
+        }
+
+        public bool RemoveAttendee(Person attendeeToRemove)
+        {
+            if (this.m_attendees.Contains(attendeeToRemove))
+            {
+                this.m_attendees.Remove(attendeeToRemove);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LunchApp/LunchApp/LunchApp/ClassReference/Group.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LunchApp.ClassReference
+{
+    public class Group : IDedNamedClass
+    {
+        public List<Person> m_groupees;
+        public List<Event> m_events;
+
+        public Group(string name)
+        {
+            this.m_name = name;
+
+            this.m_groupees = new List<Person>();
+            this.m_events = new List<Event>();
+        }
+
+        public void PayMoney(Guid toPerson, Guid fromPerson, double moneyGiven)
+        {
+            //TODO: Do some more money stuff here.
+        }
+
+        public bool RemoveGroupee(Person groupeeToRemove)
+        {
+            if (this.m_groupees.Contains(groupeeToRemove))
+            {
+                this.m_groupees.Remove(groupeeToRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveEvent(Event eventToRemove)
+        {
+            if (this.m_events.Contains(eventToRemove))
+            {
+                this.m_events.Remove(eventToRemove);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
