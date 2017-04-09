9860991c4399ccc85a8e0cdbac5e9c64df317c47
Spyder00777 <spyder00777@gmail.com>
2017-04-09 01:05:05 -0400

    MoreHelperFunctions

--- LunchApp/LunchApp/LunchApp/ServerCommunication.cs
+++ LunchApp/LunchApp/LunchApp/ServerCommunication.cs
@@ -8,21 +8,43 @@ namespace LunchApp
 {
     public static class ServerCommunication
     {
+        private static List<String> CreateLocations()
+        {
+            List<String> returnLocations = new List<String>();
+            for (int x = 0; x < 10; x++)
+            {
+                returnLocations.Add("Location " + x);
+            }
+            return returnLocations;
+        }
+
+        private static List<Order> CreateOrders()
+        {
+            return new List<Order>();
+        }
+
         private static List<Person> CreatePeople()
         {
             return new List<Person>();
         }
 
-        private static List<Event> CreateEvent()
+        private static List<Event> CreateEvents()
         {
-            List<Group> returnGroup = CreateGroup();
-            return new List<Event>();
+            List<String> returnLocations = CreateLocations();
+            List<Order> returnOrders = CreateOrders();
+            List<Person> returnPeople = CreatePeople();
+            List<Event> returnEvent = new List<Event>();
+            for (int x = 0; x < 10; x++)
+            {
+                returnEvent.Add(new Event(("Event " + x), returnLocations[x], DateTime.Today));
+            }
+            return returnEvent;
         }
 
-        private static List<Group> CreateGroup()
+        private static List<Group> CreateGroups()
         {   
             List<Person> returnPerson = CreatePeople();
-            List<Event> returnEvent = CreateEvent();
+            List<Event> returnEvent = CreateEvents();
             Guid.NewGuid();
             List<Group> returnGroups = new List<Group>();
             for (int x = 0; x < 10; x++)
