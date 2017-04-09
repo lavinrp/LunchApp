1fe3591be01c064d31e3524aa4fb92bf232fc51c
Spyder00777 <spyder00777@gmail.com>
2017-04-09 00:15:30 -0400

    i suck

--- LunchApp/LunchApp/LunchApp/Default.aspx.cs
+++ LunchApp/LunchApp/LunchApp/Default.aspx.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LunchApp.ClassReference;
 
 namespace LunchApp
 {
     public partial class _Default : Page
     {
+        Event defualtDailyEvent;
         protected void Page_Load(object sender, EventArgs e)
         {
+            defualtDailyEvent = ServerCommunication.getDailyEvent(Guid.NewGuid());
+            TextBox1.Text = defualtDailyEvent.m_name;
+        }
 
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
---
+++ LunchApp/LunchApp/LunchApp/ServerCommunication.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LunchApp.ClassReference;
+
+namespace LunchApp
+{
+    public static class ServerCommunication
+    {
+        private static List<Person> CreatePeople()
+        {
+            return new List<Person>();
+        }
+
+        private static List<Group> CreateGroup()
+        {
+            return new List<Group>();
+        }
+
+        private static List<Event> CreateEvent()
+        {
+            return new List<Event>();
+        }
+
+        public static Event getDailyEvent(Guid personalID)
+        {
+            return new Event("Wednesday Foo", "CHIPOTLE", DateTime.Today);
+        }
+
+        public static Group getGroupName(Guid personalID)
+        {
+            Group returnGroup = new Group("Cracker Barrel Group");
+            Event returnEvent = new Event("Wednesday Foo", "CHIPOTLE", DateTime.Today);
+            returnGroup.m_events.Add(returnEvent);
+            Person Chris = new Person("Chris");
+            Person Brian = new Person("Brian");
+            Person Ryan = new Person("Ryan");
+            returnGroup.m_groupees.Add(Chris);
+            returnGroup.m_groupees.Add()
+            returnGroup.m_groupees.Add(Ryan);
+            return 
+        }
+
+        public static 
+    }
+}
